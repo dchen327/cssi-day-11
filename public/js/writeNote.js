4a8bb77425e0b6420535cb10a04e22286cffe4b4
David Chen <davidc3287@gmail.com>
2021-07-26 18:29:31 +0000

    write to db and clear input fields

--- public/js/writeNote.js
+++ public/js/writeNote.js
@@ -2,30 +2,38 @@ let googleUser = null;
 
 window.onload = () => {
 
-    
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            // this code runs if the user is logged in
-            console.log("logged in as", user.displayName);
-            googleUser = user;
-        }
-        else {
-            // this code runs if the user is not logged in
-            console.log("not logged in");   
-        }
-    })
 
-    const createNoteButton = document.querySelector("#createNoteButton");
-    createNoteButton.addEventListener("click", () => {
-        // get values out of the form
-        const noteTitle = document.querySelector("#noteTitle").value;
-        const noteText = document.querySelector("#noteText").value;
-        console.log(noteTitle, noteText);
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      // this code runs if the user is logged in
+      console.log("logged in as", user.displayName);
+      googleUser = user;
+    }
+    else {
+      // this code runs if the user is not logged in
+      console.log("not logged in");
+    }
+  })
 
-        // write these values to the database
-        firebase.database().ref(`users/${googleUser.uid}`).push({
-            title: noteTitle,
-            text: noteText,
-        });
+  const createNoteButton = document.querySelector("#createNoteButton");
+  createNoteButton.addEventListener("click", () => {
+    // get values out of the form
+    const noteTitle = document.querySelector("#noteTitle").value;
+    const noteText = document.querySelector("#noteText").value;
+    console.log(noteTitle, noteText);
+
+    // write these values to the database
+    firebase.database().ref(`users/${googleUser.uid}`).push({
+      title: noteTitle,
+      text: noteText,
     })
+      .then(() => {
+        console.log("db write successful");
+        document.querySelector("#noteTitle").value = "";
+        document.querySelector("#noteText").value = "";
+      })
+      .catch(error => {
+        console.log("error writing new note:", error);
+      });
+  })
 }
